cbe4ae176c598b342ca44b905e2e9ae9846535cd
Kieran O'Leary <kieran.o.leary@gmail.com>
2017-02-15 00:09:06 +0000

    adds watch.py to repo

---
+++ watch.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import subprocess
+import time
+import shutil
+
+def check(dirname):
+    while True:
+        ls = os.listdir(dirname)
+        if len(ls) > 0:
+            for i in ls:
+                if os.path.isfile(os.path.join(dirname,i)):
+                    print i, 'found!'
+                    subprocess.call(['ffmpeg', '-i', i])
+                    shutil.move(os.path.join(dirname, i), os.path.join(dirname, 'completed'))
+        time.sleep(10)
+
+def main():
+    try:
+        os.makedirs(os.path.join(sys.argv[1], 'completed'))
+    except:
+        OSError
+    check(sys.argv[1])
+if __name__ == '__main__':
+    main()
